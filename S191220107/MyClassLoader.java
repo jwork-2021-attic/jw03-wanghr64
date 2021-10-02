1e67162e2de6e4e7f3035e8d96dc52ad76395657
wanghr <239955612@qq.com>
2021-10-02 22:50:33 +0800

    handle extra conditions & implement main()

--- S191220107/MyClassLoader.java
+++ S191220107/MyClassLoader.java
@@ -2,10 +2,19 @@ package S191220107;
 
 import java.io.*;
 import java.util.*;
+import example.encoder.SteganographyEncoder;
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 
 public class MyClassLoader extends ClassLoader {
-    public byte[] getBytesFromClassName(String name) {
-        return mClassBytes.get(name);
+    public byte[] getBytesFromClassName(String name) throws Exception {
+        if (mClassBytes.containsKey(name))
+            return mClassBytes.get(name);
+        else {
+            this.findClass(name);
+            return mClassBytes.get(name);
+        }
     }
 
     private Map<String, byte[]> mClassBytes;
@@ -37,6 +46,24 @@ public class MyClassLoader extends ClassLoader {
         }
         buffer = byteStream.toByteArray();
         return buffer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        File f = new File("./S191220107/pics/original.png");
+        File f1 = new File("./S191220107/pics/bubble.png");
+        File f2 = new File("./S191220107/pics/heap.png");
+        f1.createNewFile();
+        f2.createNewFile();
+        BufferedImage bufferedImage;
+        bufferedImage = ImageIO.read(f);
+
+        SteganographyEncoder encoder = new SteganographyEncoder(bufferedImage);
+        MyClassLoader loader = new MyClassLoader();
+        BufferedImage bi1 = encoder.encodeByteArray(loader.getBytesFromClassName("S191220107.sorter.HeapSorter"));
+        BufferedImage bi2 = encoder.encodeByteArray(loader.getBytesFromClassName("S191220107.sorter.QuickSorter"));
+
+        ImageIO.write(bi1, "png", f1);
+        ImageIO.write(bi2, "png", f2);
     }
 
 }
