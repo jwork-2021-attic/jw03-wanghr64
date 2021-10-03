ebc6847de7a11df3eb69dceae9056fe3b2ec1d15
wanghr <239955612@qq.om>
2021-10-03 14:28:47 +0800

    add File constructor

--- example/classloader/SteganographyClassLoader.java
+++ example/classloader/SteganographyClassLoader.java
@@ -1,6 +1,7 @@
 package example.classloader;
 
 import java.io.IOException;
+import java.io.File;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -12,22 +13,37 @@ import java.awt.image.BufferedImage;
 public class SteganographyClassLoader extends ClassLoader {
 
     private URL url;
+    private File file;
 
     public SteganographyClassLoader(URL imageURL) {
-        super();
-        this.url = imageURL;
+        this(imageURL, null);
     }
 
-    public SteganographyClassLoader(URL imageURL, ClassLoader parent) {
+    public SteganographyClassLoader(File file) {
+        this(null, file);
+    }
+
+    public SteganographyClassLoader(URL imageURL, File file) {
+        super();
+        this.url = imageURL;
+        this.file = file;
+    }
+
+    public SteganographyClassLoader(URL imageURL, File file, ClassLoader parent) {
         super(parent);
         this.url = imageURL;
+        this.file = file;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
 
         try {
-            BufferedImage img = ImageIO.read(url);
+            BufferedImage img;
+            if (url != null)
+                img = ImageIO.read(url);
+            else
+                img = ImageIO.read(file);
 
             SteganographyEncoder encoder = new SteganographyEncoder(img);
             byte[] bytes = encoder.decodeByteArray();
@@ -38,6 +54,5 @@ public class SteganographyClassLoader extends ClassLoader {
         }
 
     }
-
 
 }
